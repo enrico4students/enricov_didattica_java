b81f4dde3c8d40a2b9e49b640bb34751cf11df6e
enrico200165 <enrico200165@gmail.com>
2023-01-30 09:45:47 +0100

    first commit

---
+++ src/com/enrico2001665/dattica/tpsit/y4/lab10/ContaINani1.java
@@ -0,0 +1,11 @@
+package com.enrico2001665.dattica.tpsit.y4.lab10;// utilizzato in com.enrico2001665.dattica.tpsit.y4.lab10.ProvaNani1
+
+public class ContaINani1 extends Thread{
+    public void run(){
+        setName("settenani");
+        System.out.println("thread name: "+Thread.currentThread().getName());
+        for(int i = 0; i < 7; i++){
+            System.out.print("nano "+(i + 1)+", ");
+        }
+    }
+}
---
+++ src/com/enrico2001665/dattica/tpsit/y4/lab10/ContaINani2.java
@@ -0,0 +1,15 @@
+package com.enrico2001665.dattica.tpsit.y4.lab10;// thread con parametro il nome dello stesso
+
+public class ContaINani2 extends Thread{
+  public ContaINani2(String nome){                // costruttore
+    super();
+    setName(nome);  
+  }
+  
+  public void run(){
+    for(int i=0;i<7;i++){
+      System.out.println((i+1)+" "+getName());
+    }
+  }
+}
+
